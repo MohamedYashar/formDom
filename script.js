66b7606f5cf27fecbbf2e04e10d60bbf4af350c5
MohamedYashar <83939080+MohamedYashar@users.noreply.github.com>
2021-08-26 21:08:40 +0530

    v1

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 //  create function to insert dynamic data in table
-
-
-// HTML FORM VARIABLE
-//  step 1 link html input with id ti JS
 var fname   = document.getElementById("fname")
 var lname   = document.getElementById("lname")
 var Address = document.getElementById("Address")
@@ -12,28 +8,14 @@ var food    = document.getElementById("food")
 var state   = document.getElementById("state")
 var country = document.getElementById("country")
 
-/* step 2 use the above vaviable  to insert 
-the eneterd data dynamicllyfrom form to table*/
-
-//  CREATE FUNCTION USE ABOVE VAR AS PARAMETER=> NOW IT ACTS AS BRIDGE FOR HTML AND JS INPUT DATA
 
 function TableData(fname,lname,Address,pin,gender,food,state,country){
     console.log(fname);
-    
 
-// 1.CREATE VAR SEPARATELY FOR TABLE ROW AND TABLE DATE AND THEIR INNER TEXT = ABOVE INPUT VAR
-// 2. WHEN U ENTER DATE IN FORM IT GOES TO TABLE
-
-
-// TABLE VARIABLE
-
-// use tbody id from html
 var tbody = document.getElementById("tbody")
 
-//below var to create dynamic data
-// created trow
 var trow  = document.createElement("tr")
-// created tdata
+
 var tdata1 = document.createElement("td")
 var tdata2 = document.createElement("td")
 var tdata3 = document.createElement("td")
@@ -71,19 +53,16 @@ var button = document.getElementById("submit");
 button.addEventListener("click",function(x){
     x.preventDefault();
 
-    // FOR GENDER INPUT IN FORM
 var gender = document.getElementsByName("gender");
 console.log(gender);
 var inputvalue;
 for(i=0;i<gender.length;i++){
     if(gender[i].checked){
-        //console.log(gender[i].checked);
         inputvalue = gender[i].value;
-       // console.log(inputvalue);
+       
     }
 }
 
-// FOR FOOD INPUT 
 
 var food =document.getElementsByName("food")
 console.log(food);
@@ -93,14 +72,13 @@ var count =0;
 for(i=0;i<food.length;i++){
     if(food[i].checked){
         box.push(food[i].value);
-        count++;
-        //console.log(box);
+        count++;       
     }
 }
 
 if (box.length>=2){
     var output =box.join(",");
-    //console.log(output);
+    
 }else{
     output= alert("Choose atleast 2 foods");
 }
@@ -108,15 +86,8 @@ if (box.length>=2){
 TableData(fname.value,lname.value,Address.value,pin.value,
     inputvalue,output,state.value,country.value)
 
-fname.value=""
-lname.value=""
-Address.value=""
-pin.value=""
-inputvalue=""
-output="";
-state.value=""
-country.value=""
-
+    // Reset the form once clicked
+document.getElementById("myform").reset();
 
 
 });
